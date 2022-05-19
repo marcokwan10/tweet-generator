092baceab78fdd091f5d4be7f7b7f1f79d5588cb
Marco Kwan <marcokwan10@gmail.com>
2022-05-19 17:34:15 -0400

    cleaned up commented out lines

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,7 @@ import React from "react";
 function Header() {
 	return (
 		<div className="header-container">
-			<h3>
-				{/* Tweet Generator ____<span id="blink">I</span> */}
-				Tweet Generator +
-			</h3>
+			<h3>Tweet Generator +</h3>
 		</div>
 	);
 }
